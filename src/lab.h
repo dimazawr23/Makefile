9c9f43d6dbf425cd01d8ce43b4094dff063a0494
dimazawr23 <dimazawr24@gmail.com>
2016-06-13 04:54:18 +0600

    add lib lab.h

---
+++ src/lab.h
@@ -0,0 +1,15 @@
+void sdvig(char *s, int j, int i, char delim, char *ptr[]);
+int isLowCase(char ch);
+int isHighCase(char ch);
+void change(char *s, int k);
+void sspn(char *s);
+int slen(char *s);
+int stok(char *s, char delim, char *ptr[]);
+int schr(char *s, char delim);
+int sequal(char *s1, char *s2);
+void scopy(char *s1, char *s2);
+void suntok(char *s, char delim, char *ptr[], int cnt);
+void input();
+void check();
+void process();
+void output();
